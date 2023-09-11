bbb6720d12092ea83e9e183d1c1b362a6fc31cba
init <16517372+init4t@users.noreply.github.com>
2023-09-11 10:46:01 +0200

    i

--- pages/poems.js
+++ pages/poems.js
@@ -6,16 +6,6 @@ import React, { useEffect } from 'react';
 
 export default function Poems({ poems }) {
 
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.src = 'https://telegram.org/js/telegram-web-app.js';
-        script.async = true;
-        document.body.appendChild(script);
-        return () => {
-          document.body.removeChild(script);
-        };
-      }, []);
-
 
     const addLineBreak = (str) =>
     str.split('\n').map((subStr, index) => (
