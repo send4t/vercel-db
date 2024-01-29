24dd65bef8333bed91e09efa248520ac7f41c204
send4t <beadatbeat@gmail.com>
2024-01-29 20:17:08 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     /* config options here */
     images: {
-        domains: ['hips.hearstapps.com', 'rainbowplantlife.com', 'image-api.nosalty.hu', 'mindmegette.hu/'],
+        domains: ['hips.hearstapps.com', 'rainbowplantlife.com', 'image-api.nosalty.hu', 'mindmegette.hu/', 'kep.index.hu/'],
     }
   }
    
